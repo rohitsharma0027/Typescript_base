b7b6cd51c419005fcdebcc2e126312bb06ac18cf
rohitsharma0027 <developer.rohitsharmaj7@gmail.com>
2021-07-03 07:11:38 +0530

    Leveraging typescript benefit

---
+++ typescript.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+var resultObtained = addNumbers('2', '5'); // will give error here only as the function demand parameters of type numbers
+console.log(resultObtained);
